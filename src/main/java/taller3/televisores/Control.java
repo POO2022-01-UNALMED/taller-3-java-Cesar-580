fec95148dd1fe36aecd50cd442655d795ff8a007
Cesar-580 <65823540+Cesar-580@users.noreply.github.com>
2022-03-28 23:52:23 -0500

    P5

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,19 +1,20 @@
 package taller3.televisores;
 
 public class Control {
-	
-	//Atributo
+
+	// Atributo
 	private TV tv;
-	
-	public void enlazar (TV televisor) {
+
+	public void enlazar(TV televisor) {
 		this.tv = televisor;
 		televisor.setControl(this);
 	}
-	
+
 	// Apagado-Encendido-Estado
-	public void turnOn () {
+	public void turnOn() {
 		tv.turnOn();
 	}
+
 	public void turnOff() {
 		tv.turnOff();
 	}
@@ -21,18 +22,29 @@ public class Control {
 	public void canalUp() {
 		tv.canalUp();
 	}
+
 	public void canalDown() {
 		tv.canalDown();
 	}
-	
+
 	public void volumenUp() {
 		tv.volumenUp();
 	}
+
 	public void volumenDown() {
 		tv.volumenDown();
 	}
-	
+
 	public void setCanal(int canal) {
 		tv.setCanal(canal);
+	}
+
+	// TV
+	public void setTV(TV televisor) {
+		tv = televisor;
+	}
+
+	public TV getTV() {
+		return tv;
 	}
 }
